95828ece6b833a2f2e78339c9891178de6b24eb8
Joker 13 <99423987+Jean13331@users.noreply.github.com>
2023-09-22 17:46:48 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+import { fastify } from 'fastify'
+import { databasePostgres } from './database-postgres.js'
+//import { databasememory} from './databasememory.js'
+//const database = new databasememory() // Esta linha está comentada, mas pode causar problemas se for descomentada sem importar o módulo necessário.
+const database = new databasePostgres()
+const server = fastify()
+
+
+server.post('/videos', async (request, response) => {
+    const { title, description, duration } = request.body
+
+    database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return response.status(201).send()
+})
+
+server.get('/videos', async (request) => {
+    const search = request.query.search
+    //console.log(search)
+
+    const videos = await database.list(search)
+
+    return videos
+})
+
+server.put('/videos/:id', async (request, response) => {
+    const videosId = request.params.id
+    const { title, description, duration } = request.body
+
+    await database.update(videosId, {
+        title,
+        description,
+        duration
+    })
+    return response.status(204).send()
+})
+
+server.delete('/videos/:id', async (request, response) => {
+    const videosId = request.params.id
+
+    await database.delete(videosId)
+
+    return response.status(204).send()
+})
+
+server.listen({
+    port: process.env.port ?? 3333,
+})
